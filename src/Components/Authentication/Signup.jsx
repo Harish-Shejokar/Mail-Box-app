8ff74e23456ab43e7ac53869770c924af98e71a8
Harish-Shejokar <90904311+Harish-Shejokar@users.noreply.github.com>
2023-05-16 10:50:33 +0530

    signUp page

---
+++ src/Components/Authentication/Signup.jsx
@@ -0,0 +1,123 @@
+import React, { useRef } from "react";
+import { Col, Button, Row, Container, Card, Form } from "react-bootstrap";
+import axios from "axios";
+
+const Signup = () => {
+  const emailRef = useRef();
+  const passRef = useRef();
+  const confirmPassRef = useRef();
+
+  const signUpOnFirbase = async (email, password) => {
+    console.log(email, password);
+    const url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAjGOUVjefbMK04WvwbU2wGP4OMSyUetJw";
+    const payLoad = {
+      email,
+      password,
+      returnSecureToken: true,
+      };
+      
+
+    try {
+      const response = await axios.post(url, payLoad);
+
+      if (response.ok) {
+        console.log("sign up successfully");
+      } else {
+        console.log("sign up not ok");
+        console.log(response);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const signupHandler = (e) => {
+    e.preventDefault();
+    const email = emailRef.current.value;
+    const pass = passRef.current.value;
+    const confirmpass = confirmPassRef.current.value;
+    if (pass !== confirmpass) {
+      alert("password not matched!!");
+      return;
+    }
+    // console.log(email, pass, confirmpass);
+
+    signUpOnFirbase(email, pass);
+  };
+
+  return (
+    <div>
+      <Container bg="dark" variant="dark" className="">
+        <Row className="vh-100 d-flex justify-content-center align-items-center">
+          <Col md={8} lg={6} xs={12}>
+            <div className="border border-2 border-primary"></div>
+            <Card className="shadow px-4">
+              <Card.Body>
+                <div className="mb-3 mt-md-4">
+                  <h2 className="fw-bold mb-2 text-center text-uppercase ">
+                    Logo
+                  </h2>
+                  <div className="mb-3">
+                    <Form>
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label className="text-center">
+                          Email address
+                        </Form.Label>
+                        <Form.Control
+                          ref={emailRef}
+                          type="email"
+                          placeholder="Enter email"
+                        />
+                      </Form.Group>
+
+                      <Form.Group
+                        className="mb-3"
+                        controlId="formBasicPassword"
+                      >
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                          ref={passRef}
+                          type="password"
+                          placeholder="Password"
+                        />
+                      </Form.Group>
+                      <Form.Group className="mb-3">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control
+                          ref={confirmPassRef}
+                          type="password"
+                          placeholder="Password"
+                        />
+                      </Form.Group>
+                      <Form.Group className="mb-3"></Form.Group>
+                      <div className="d-grid">
+                        <Button
+                          onClick={signupHandler}
+                          variant="primary"
+                          type="submit"
+                        >
+                          Create Account
+                        </Button>
+                      </div>
+                    </Form>
+                    <div className="mt-3">
+                      <p className="mb-0  text-center">
+                        Already have an account??{" "}
+                        <a href="{''}" className="text-primary fw-bold">
+                          Sign In
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Signup;
